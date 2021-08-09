c15cc687be6c2a40db700eb71cd34a5495aaf219
Talha <abutalhadanish@gmail.com>
2021-08-09 20:50:05 +0530

    Made some fields nullable

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class Department(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=5)
     chairman = models.ForeignKey('core.Employee', on_delete=models.RESTRICT, blank=True, null=True, related_name='chairman_of')
-    posting_rights = models.ManyToManyField('core.Employee', related_name='has_posting_rights_of')
+    posting_rights = models.ManyToManyField('core.Employee', related_name='has_posting_rights_of', blank=True, null=True)
 
 class User(AbstractUser):
     email_notifications = models.BooleanField(default=True)
@@ -26,8 +26,8 @@ class Employee(models.Model):
     phone = models.CharField(max_length=13)
     department = models.ForeignKey(Department, on_delete=models.RESTRICT)
     designation = models.CharField(max_length=5, choices=DesignationChoice.choices)
-    user = models.OneToOneField(User, on_delete=models.RESTRICT, related_name="employee")
+    user = models.OneToOneField(User, on_delete=models.RESTRICT, related_name="employee", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    edited_at = models.DateTimeField()
+    edited_at = models.DateTimeField(auto_now=True)
 
 
